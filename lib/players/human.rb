79e9d13b424ab08e425b0a042a09b725cb00ba24
Manny Pissanos <mpissano@gmail.com>
2018-10-22 20:42:26 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,10 +1,11 @@
+require_relative '../player.rb'
 
 
 class Players::Human < Player
-  
-  def move(input)
-    gets.chomp
-  end
-  
-end
 
+    def move(board)
+        puts "   please enter the index where you'd like to play: "
+        gets.chomp
+    end
+
+end
